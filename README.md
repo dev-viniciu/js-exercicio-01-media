c5a9e07c645def3dda507a46c69ef8da63782429
Luiz Vinicius Sousa Silva <vinciussousan10@gmail.com>
2026-07-09 16:31:30 -0300

    feat: atualiza lógica do index.js e descrição do README

---
+++ README.md
@@ -0,0 +1,1 @@
+Um script simples para calcular a média aritmética de três notas e exibir o resultado no terminal.
--- index.js
+++ index.js
@@ -5,5 +5,10 @@ let nota3 = 9;
 let somaNotas = nota1 + nota2 + nota3
 let media = somaNotas/3
 
+if (media >= 7) {
+    console.log("Aluno aprovado");
+} else {
+    console.log("Aluno reprovado");
+}
 
 console.log("A média das notas é: " + media)
